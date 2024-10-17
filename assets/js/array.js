6129ee44720f7976a6c0e6f073da6325a084298d
Haiqastackdeveloper <haiqa0021@gmail.com>
2024-10-17 23:40:21 +0500

    done

--- assets/js/array.js
+++ assets/js/array.js
@@ -1,4 +1,7 @@
 let arr = ["äli","saba","sadia","farhan","haiqa","zaid"];
+if(arr%2 == 0){
+    
 for (let i =0; i<arr.length;i++){
     document.write("<p>"+arr[i]+"</p>")
+}
 }
--- assets/js/fabonacci-series.js
+++ assets/js/fabonacci-series.js
@@ -1,9 +1,15 @@
-let a=0;
-b=1;
-c = a + b;
-d = b + c;
-e = c + d;
-f = d + e;
-g = e + f;
-h = f + g;
-document.write(a,b,c,d,e,f,g,h);
+var a=0,b=1,c="";
+// for(var i=1; i<100;i++){
+    var c=a+b;
+    d=b+c;
+    e=c+d;
+    f=d+e;
+    g=e+f;
+    h=f+g;
+    i=g+h;
+    j=h+i;
+    k=i+j;
+    l=j+k;
+document.write("<h1 style=font-size:80px;>Fabonacci Series</h1>");
+document.write(a+"&nbsp;&nbsp;&nbsp;&nbsp;",b+"&nbsp;&nbsp;&nbsp;&nbsp;",c+"&nbsp;&nbsp;&nbsp;&nbsp;",d+"&nbsp;&nbsp;&nbsp;&nbsp;",e+"&nbsp;&nbsp;&nbsp;&nbsp;",f+"&nbsp;&nbsp;&nbsp;&nbsp;",g+"&nbsp;&nbsp;&nbsp;&nbsp;",h+"&nbsp;&nbsp;&nbsp;&nbsp;",i+"&nbsp;&nbsp;&nbsp;&nbsp;",j+"&nbsp;&nbsp;&nbsp;&nbsp;",k+"&nbsp;&nbsp;&nbsp;&nbsp;",l)
+
