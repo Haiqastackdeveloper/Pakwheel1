70cab97bec9e0ff01cef5a5881bf23f73828294e
Haiqastackdeveloper <haiqa0021@gmail.com>
2024-10-18 19:22:29 +0500

    done

---
+++ assets/js/fizzbuzz.js
@@ -0,0 +1,7 @@
+let number1 = parseInt(prompt("enter the number where fizzbuzz game start"));
+let number2 = parseInt(prompt("enter the number where fizzbuzz game end"));
+for (var i = number1; i <=number2 ; i++) {
+    if (i % 5 == 0)  document.write("<h3 style='background-color:blue;color:white;text-align:center;'>"+i+"FizzBuzz</h3>");
+    else if (i % 2 == 0) document.write("<h3 style='background-color:red;color:white;text-align:center;'>"+i+"Fizz</h3>");
+    else  document.write("<h3 style='background-color:green;color:white;text-align:center;'>"+i+"Buzz</h3>");
+}
